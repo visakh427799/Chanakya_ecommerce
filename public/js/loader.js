9cdff167e4271f80462cda02db35409f3719a73c
visakh427799 <visakhsanthosh69@gmail.com>
2020-12-14 10:09:11 +0530

    sixth commit

--- public/js/loader.js
+++
@@ -1,8 +0,0 @@
-	var Preloader=document.getElementById("loader");
-		function delay(){
-            setTimeout(show,3000);
-        }
-            function show(){
-			Preloader.style.display='none';
-			}
-				
